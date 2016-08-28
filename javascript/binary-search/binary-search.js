566f6b3883efa86bceefdaa4f08e4c7e7e8e2910
Brian Rippeto <rippeto@gmail.com>
2016-08-27 22:23:46 -0500

    js binary search complete

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,22 +1,39 @@
 class BinarySearch {
   constructor(data) {
-    this.array = validateData(data)
+    this.array = validateData(data);
+  }
+
+  indexOf(value) {
+    return this.array.lastIndexOf(value);
   }
 }
 
+function findMiddleIndex(data) {
+  var i = data.length;
+  var middleIndex = Math.round(i/2)
+  return middleIndex
+}
+
 function validateData(data) {
-  var sortedShit = data.concat().sort(function (a, b) {  return a - b;  })
-  console.log(data);
-  console.log(sortedShit);
-  if (sortedShit === data) {
+  var sortedData = data.concat().sort(function (a, b) {  return a - b;  })
+  if (identicalArrays(data, sortedData)) {
     return data
   } else {
     return undefined
   }
 }
 
-// function sortNumber(a, b) {
-//     return a - b;
-// }
+function identicalArrays(original, sorted) {
+  var i = original.length;
+  if (i !== sorted.length) {
+    return false;
+  }
+  while (i--) {
+    if (original[i] !== sorted[i]) {
+      return false;
+    }
+  }
+  return true
+}
 
 module.exports = BinarySearch
